20136a4148629cc920a5afc4146d4ebbeca5185d
Joeri Harreman <142898105+JoeriH06@users.noreply.github.com>
2025-09-30 11:34:46 +0200

    final details to home and plot page

--- Assignment1/streamlit/pages/3_Context.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Context")
-st.write("📑 This is the Context page.")
--- Assignment1/streamlit/streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="IND320 Project", page_icon="📊", layout="wide")
-
-st.title("Home")
-st.write("👋 Welcome to the IND320 Project homepage!")
-
-st.info("This is the *homepage*.")
